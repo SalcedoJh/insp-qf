4fc35bf5808111bbbf80ea34818dacf3170e9eb7
jhomerson salcedo <jhomerson05@gmail.com>
2025-07-10 00:30:09 -0500

    se modifico el config

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-  host: '0.0.0.0', // <- esto permite conexiones externas
-  port: 5173,
-  strictPort: true, // evita que cambie el puerto
-  },
-  
+  plugins: [react()],  
 })
